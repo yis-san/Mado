7479bf7816955a1669298bbc50cb5d0639371346
Bartek <75677928+yis-san@users.noreply.github.com>
2021-02-21 15:53:09 +0100

    Update buttonsmain.js

--- buttonsmain.js
+++ buttonsmain.js
@@ -1,24 +1,24 @@
 function rtask1() {
-    window.open("subsites/rtask1.html")
+    window.open("rtask1.html")
 }
 function rtask2() {
-    window.open("subsites/rtask2.html")
+    window.open("rtask2.html")
 }
 function rtask3() {
-    window.open("subsites/rtask3.html")
+    window.open("rtask3.html")
 }
 function rtask4() {
-    window.open("subsites/rtask4.html")
+    window.open("rtask4.html")
 }
 function rtask5() {
-    window.open("subsites/rtask5.html")
+    window.open("srtask5.html")
 }
 function rtask6() {
-    window.open("subsites/rtask6.html")
+    window.open("rtask6.html")
 }
 function rtask7() {
-    window.open("subsites/rtask7.html")
+    window.open("rtask7.html")
 }
 function rtask8() {
-    window.open("subsites/rtask8.html")
+    window.open("rtask8.html")
 }
